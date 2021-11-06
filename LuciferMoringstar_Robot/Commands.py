59c3f06777273c1f764130bac162f3b63582541b
aslamcreation12 <89309015+aslamcreation12@users.noreply.github.com>
2021-11-06 20:22:11 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 developer  🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 Developer  🎖', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
